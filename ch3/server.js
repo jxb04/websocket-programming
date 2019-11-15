4f9cd9df7db7095f1d3fab6a58e54415c79d241d
Joel Bernasky <joel_bernasky@hotmail.com>
2019-11-15 12:28:36 -0500

    refactor variable initialization

--- ch3/server.js
+++ ch3/server.js
@@ -1,23 +1,33 @@
+var WebSocket = require('ws');
+var WebSocketServer = WebSocket.Server,
+    wss = new WebSocketServer({ port: 8181 });
 var uuid = require('node-uuid');
 
-var WebSocketServer = require('ws').Server,
-   wss = new WebSocketServer({port: 8181});
+var clients = [];
 
-   var clients = [];
+wss.on('connection', function (ws) {
+    var client_uuid = uuid.v4();
+    clients.push({ "id": client_uuid, "ws": ws });
+    console.log('client [%s] connected', client_uuid);
 
-   wss.on('connection', function(ws) {
-       var client_uuid = uuid.v4();
-       clients.push({"id": client_uuid, "ws": ws});
-       console.log('client [%s] connected', client_uuid);
-    
+    ws.on('message', function (message) {
+        for (var i = 0; i < clients.length; i++) {
+            var clientSocket = clients[i].ws;
+            if (clientSocket.readyState === WebSocket.OPEN) {
+                console.log('client [%s]: %s', clients[i].id, message);
+                clientSocket.send(JSON.stringify({
+                    "id": client_uuid,
+                    "message": message
+                }));
+            }
+        }
+    });
 
-       ws.on('message', function(message) {
-           for (var i = 0; i<clients.length; i++) {
-               var clientSocket = clients[i].ws;
-               console.log('client [%s]: %s', clients[i].id, message);
-               clientSocket.send(JSON.stringify({
-                   "id": client_uuid, "message": message
-               }));
-           }
-       });
-   });
+    ws.on('close', function() {
+        for(var i=0; i<clients.length; i++) {
+            if (clients[i].id == client_uuid) {
+                console.log('client [%s} disconnected', client_uuid);
+                clients.splice(i, 1);
+            }
+        }
+    });
