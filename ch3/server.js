4e3450c501b3d2f2b368563c668dcdc9bf5ab690
Joel Bernasky <joel_bernasky@hotmail.com>
2019-11-15 15:16:07 -0500

    add nickname to clienta

--- ch3/server.js
+++ ch3/server.js
@@ -5,6 +5,26 @@ var uuid = require('node-uuid');
 
 var clients = [];
 
+var nickname = client_uuid.substr(0,8);
+clients.push({"id": client_uuid, "ws": ws, "nickname": nickname});
+
+if (message.indexOf('/nick') == 0) {
+    var nickname_array = message.split(' ')
+    if (nickname_array.length >= 2) {
+        var old_nickname = nickname;
+        nickname = nickname_array[1];
+        for (var i=0; i<clients.length; i++) {
+            var clientSocket = clients[i].ws;
+            var nickname_message = "Client " + old_nickname + " changed to " + nickname;
+            clientSocket.send(JSON.stringify({
+                "id": client_uuid,
+                "nickname": nickname,
+                "message": nickname_message
+            }));
+        }
+    }
+}
+
 wss.on('connection', function (ws) {
     var client_uuid = uuid.v4();
     clients.push({ "id": client_uuid, "ws": ws });
@@ -17,6 +37,7 @@ wss.on('connection', function (ws) {
                 console.log('client [%s]: %s', clients[i].id, message);
                 clientSocket.send(JSON.stringify({
                     "id": client_uuid,
+                    "nickname": nickname,
                     "message": message
                 }));
             }
@@ -39,3 +60,5 @@ wss.on('connection', function (ws) {
         message.innerHTML = data.message;
         messages.appendChild(message);
     }
+
+})
